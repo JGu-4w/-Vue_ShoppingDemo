8327cb66125afe5a0c9f748dcbeb7c336a0732e7
JGu-4w <joegu.4work@gmail.com>
2020-06-27 22:47:16 +0800

    ToDo: 获取products数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,47 +6,185 @@
       </template>
     </nav-bar>
     <swiper :banners="banners"></swiper>
-    <reco-view :recommends="recommends"></reco-view>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles='["流行", "新款", "精选"]'></tab-control>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+      <li>31</li>
+      <li>32</li>
+      <li>33</li>
+      <li>34</li>
+      <li>35</li>
+      <li>36</li>
+      <li>37</li>
+      <li>38</li>
+      <li>39</li>
+      <li>40</li>
+      <li>41</li>
+      <li>42</li>
+      <li>43</li>
+      <li>44</li>
+      <li>45</li>
+      <li>46</li>
+      <li>47</li>
+      <li>48</li>
+      <li>49</li>
+      <li>50</li>
+      <li>51</li>
+      <li>52</li>
+      <li>53</li>
+      <li>54</li>
+      <li>55</li>
+      <li>56</li>
+      <li>57</li>
+      <li>58</li>
+      <li>59</li>
+      <li>60</li>
+      <li>61</li>
+      <li>62</li>
+      <li>63</li>
+      <li>64</li>
+      <li>65</li>
+      <li>66</li>
+      <li>67</li>
+      <li>68</li>
+      <li>69</li>
+      <li>70</li>
+      <li>71</li>
+      <li>72</li>
+      <li>73</li>
+      <li>74</li>
+      <li>75</li>
+      <li>76</li>
+      <li>77</li>
+      <li>78</li>
+      <li>79</li>
+      <li>80</li>
+      <li>81</li>
+      <li>82</li>
+      <li>83</li>
+      <li>84</li>
+      <li>85</li>
+      <li>86</li>
+      <li>87</li>
+      <li>88</li>
+      <li>89</li>
+      <li>90</li>
+      <li>91</li>
+      <li>92</li>
+      <li>93</li>
+      <li>94</li>
+      <li>95</li>
+      <li>96</li>
+      <li>97</li>
+      <li>98</li>
+      <li>99</li>
+      <li>100</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
 import Swiper from 'components/common/swiper/Swiper';
-import RecoView from './childComponents/HomeRecommendView';
 
-import { getHomeMultidata } from 'network/home'
+import TabControl from 'components/content/tabControl/TabControl';
+
+import RecommendView from './childComponents/HomeRecommendView';
+import FeatureView from './childComponents/FeatureView';
+
+import { getHomeMultidata, getHomeProducts } from 'network/home'
 
 export default {
   name: 'home',
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      products: {
+        'pop': {page:0, list:[]},
+        'new': {page:0, list:[]},
+        'sell': {page:0, list:[]},
+      }
     }
   },
   components: {
     NavBar,
     Swiper,
-    RecoView,
+    TabControl,
+    RecommendView,
+    FeatureView,
   },
   created() {
-    getHomeMultidata()
-      .then(res => {        
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
-      .catch(err => console.log(err))
+    this.getHomeMultidata();
+    this.getHomeProducts('pop');
+    this.getHomeProducts('new');
+    this.getHomeProducts('sell');
+  },
+  methods: {
+    /**
+     * 获取轮播图和热门推荐数据
+     */
+    getHomeMultidata() {
+      getHomeMultidata()
+        .then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+        .catch(err => console.log(err));
+    },
+    /**
+     * 获取流行，新款和精选商品的数据
+     */
+    getHomeProducts(type) {
+      const page = this.products[type].page + 1;
+      getHomeProducts(type, page).then(res => {
+        console.log(res);
+        this.products[type].list.push(...res.data.list);
+        this.products[type].page++;
+      }).catch(err => console.log(err));
+    }
   }
 }
 </script>
 
-<style>
-  #home {
-    height: 100vh;
-  }
+<style scoped>
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
