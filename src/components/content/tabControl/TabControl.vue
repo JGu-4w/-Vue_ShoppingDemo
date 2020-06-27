470e3fbefa8e3caacbcf3d0e79693f421597d180
JGu-4w <joegu.4work@gmail.com>
2020-06-27 22:44:18 +0800

    新增导航通用组件

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-control">
+    <div class="title-list" 
+        v-for="(item, index) in titles" :key="index" 
+        :class="{active: index === currentIndex}" 
+        @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    }
+  },
+  methods: {
+    itemClick: function(index) {
+      this.currentIndex = index;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+    font-size: 14px;
+    margin: 4px 0;
+    background-color: #fff;
+  }
+
+  .title-list {
+    flex: 1;
+  }
+
+  .title-list span {
+    padding: 10px;
+  }
+
+  .active {
+    color: var(--color-high-text);
+  }
+
+  .active span {
+    border-bottom: 2px solid var(--color-high-text);
+  }
+</style>
