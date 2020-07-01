1375e814fd9024cb1b2af0edd71a6e9dc3fd760a
JGu-4w <joegu.4work@gmail.com>
2020-07-01 16:24:18 +0800

    样式修改

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -19,7 +19,7 @@ export default {
     line-height: 44px;
     text-align: center;
     z-index: 9;
-    /* box-shadow: 0 1px 1px rgba(100, 100, 100, .1); */
+    box-shadow: 0 1px 1px rgba(100, 100, 100, .1);
   }
 
   .left {
